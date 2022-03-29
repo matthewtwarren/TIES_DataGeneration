80c902da411228867ca4e41352eda83f0b5d3a55
matthewtwarren <mattwarren95@gmail.com>
2022-03-29 18:10:31 +0100

    Added example run file

---
+++ run.sh
@@ -0,0 +1,3 @@
+rm -r output
+
+python generate_data.py --tablesets 5 --minrows 4 --maxrows 10 --mincols 4 --maxcols 10 --threads 1 --outpath /Users/matt/Git/TableGeneration/output --visualizeimgs 1 --visualizebboxes 1
